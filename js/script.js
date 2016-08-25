ec3a16911b5cb6b9f43e85101ed4f41b314799d8
Garrick <cerulean808@hotmail.com>
2016-08-25 14:02:02 +1200

    This is first commit for remote repository kiwi-lifter.gihub.io.

--- js/script.js
+++ js/script.js
@@ -1,6 +1,4 @@
-
 function loadData() {
-
     var $body = $('body');
     var $wikiElem = $('#wikipedia-links');
     var $nytHeaderElem = $('#nytimes-header');
@@ -12,8 +10,15 @@ function loadData() {
     $nytElem.text("");
 
     // load streetview
-
-    // YOUR CODE GOES HERE!
+	var streetStr = $('#street').val();
+	var cityStr = $('#city').val();
+    var address = streetStr + ', ' + cityStr;
+	
+	$greeting.text('Does ' + address + ' seem like a nice place to holiday?');
+	
+	var streetViewUrl = 'http://maps.googleapis.com/maps/api/streetview?size=600x400&location=' + address + '';
+	$body.append('<img class="bgimg" src="' + streetviewUrl + '">');
+	
 
     return false;
 };
