a47b4524139ec7dff14c67f63c097725857a07f1
Garrick <cerulean808@hotmail.com>
2016-08-26 12:14:17 +1200

    This comments out NYTimes api code to try and fix bug.

--- js/script.js
+++ js/script.js
@@ -19,7 +19,7 @@ function loadData(e) {
 	
 	var streetViewUrl = 'http://maps.googleapis.com/maps/api/streetview?size=600x400&location=' + address + '';
 	$body.append('<img class="bgimg" src="' + streetViewUrl + '">');
-
+	/**
 	//NYTimes Ajax request. Pretty please. 
      var timesURL = 'https://api.nytimes.com/svc/search/v2/articlesearch.json?q=' +
         cityStr + '&sort=newest&api-key=d785b92816364c388bda7f12d75af739';
@@ -42,7 +42,7 @@ function loadData(e) {
 				console.log('error!!!!');
 				$nytHeaderElem.text("Error with request.");
 			});
-	
+	**/
 };
 
 $('#form-container').submit(loadData);
