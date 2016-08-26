eaded67374c27691aa357efa3c273448aeb83ad1
Garrick <cerulean808@hotmail.com>
2016-08-26 12:06:12 +1200

    This fixes a variable name that caused error.

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
-function loadData() {
+function loadData(e) {
+	e.preventDefault();
     var $body = $('body');
     var $wikiElem = $('#wikipedia-links');
     var $nytHeaderElem = $('#nytimes-header');
@@ -18,7 +19,7 @@ function loadData() {
 	
 	var streetViewUrl = 'http://maps.googleapis.com/maps/api/streetview?size=600x400&location=' + address + '';
 	$body.append('<img class="bgimg" src="' + streetViewUrl + '">');
-	
+
 	//NYTimes Ajax request. Pretty please. 
      var timesURL = 'https://api.nytimes.com/svc/search/v2/articlesearch.json?q=' +
         cityStr + '&sort=newest&api-key=d785b92816364c388bda7f12d75af739';
@@ -36,7 +37,12 @@ function loadData() {
                      '</li>');
              }
                  return false;
-         });
+
+		 }).fail(function(e){
+				console.log('error!!!!');
+				$nytHeaderElem.text("Error with request.");
+			});
+	
 };
 
 $('#form-container').submit(loadData);
